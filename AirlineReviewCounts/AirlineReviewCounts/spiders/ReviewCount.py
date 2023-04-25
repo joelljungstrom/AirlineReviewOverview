dd88b58789f57731b7174648283225056d346f6d
Joel Ljungström <104433520+joelljungstrom@users.noreply.github.com>
2023-04-25 10:54:41 +0200

    Original files

---
+++ AirlineReviewCounts/AirlineReviewCounts/spiders/ReviewCount.py
@@ -0,0 +1,45 @@
+# Import library
+import scrapy
+import re
+from AirlineReviewCounts.items import AirlinereviewcountsItem
+
+# Create Spider class
+class ReviewCountCrawler(scrapy.Spider):
+    # Name of spider
+    name = 'ReviewCountCrawler'
+    # Website to scrape
+    allowed_urls = ['https://www.airlinequality.com/']
+    start_urls = [
+        'https://www.airlinequality.com/review-pages/a-z-airline-reviews/airline-review-ratings/'
+    ]
+
+    # Parses the website
+    def parse(self, response):
+        counts = response.xpath('//*/div[@class="aggregateRow clearfix"]')
+        for count in counts:
+            # Airline Name
+            try:
+                name = count.xpath('.//div[@class="aggregateColumn aggregateColumn_two"]/a/text()').extract_first()
+            except:
+                name = ''
+
+            # Slug
+            try:
+                link = count.xpath('.//div[@class="aggregateColumn aggregateColumn_two"]/a/@href')
+                slug = re.search(r'/airline-reviews/(.+?)$', link.get()).group(1)
+            except:
+                slug = ''
+
+            # Number of Reviews
+            try:
+                count = count.xpath('.//div[@class="aggregateColumn aggregateColumn_four"]/text()').extract_first()
+            except:
+                count = ''
+
+            item = AirlinereviewcountsItem()
+            item['AirlineName'] = name
+            item['Slug'] = slug
+            item['Reviews'] = count
+
+            yield item
+        pass
